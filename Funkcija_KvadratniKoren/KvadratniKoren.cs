58e8e1850be43bde50c5d33f031c16cf42276720
Tatjana Kovacevic <tatkovace@outlook.com>
2019-04-05 12:36:30 +0200

    Add project files.

---
+++ Funkcija_KvadratniKoren/KvadratniKoren.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funkcija_KvadratniKoren
+{
+    class KvadratniKoren
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This applicaton calls method wich calculate square root of positive part of given number by interpolation.\n");
+            Console.Write("Insert number: ");
+            double n = Double.Parse(Console.ReadLine());
+
+            string b = "";
+            if (n < 0)
+                b = b + "i";
+
+            Console.WriteLine("\nSquare root of given number is: " + KvKor(n) + b);
+
+            Console.ReadKey();
+
+            double KvKor (double a)
+            {
+                if (a < 0)
+                    a = a * (-1);
+                
+                string A = a.ToString();
+                double k = 1;
+
+                if (a >= 1)
+                {
+                    int m = (A.Length + 1) / 2;
+                    for (int j = 1; j <= m; j++)
+                        k = k * 10;
+                }
+
+                double s = 0;
+                for (int j = 0; j < 17; j++)
+                {
+                    for (int i = 0; i <= 10; i++)
+                    {
+                        if ((s + k * i) * (s + k * i) > a)
+                        {
+                            s = s + k * (i - 1);
+                            break;
+                        }
+                    }
+                    if (s*s == a)
+                        break;
+                    k = k / 10;
+                }
+                return s;
+            }
+
+        }
+    }
+}
---
+++ Funkcija_KvadratniKoren/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Funkcija_KvadratniKoren")]
+[assembly: AssemblyDescription("Square root of number N by interpolation")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("tatkovace@outlook.com tatkovace@gmail.com")]
+[assembly: AssemblyProduct("Funkcija_KvadratniKoren")]
+[assembly: AssemblyCopyright("Copyright © TatKovace 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7cb9680e-5fad-48f6-bda8-929d9cf1bd0d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
